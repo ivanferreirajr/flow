e7e1723c4ffca4ff8e1136055d1114c8caacc6c2
ivanferreirajr <ivan.junior@dcomp.ufs.br>
2021-02-27 08:17:37 -0300

    feat: using Context API to countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import Head from 'next/head';
+
 import React from "react";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
+import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengeBox } from "../components/ChallengeBox";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -17,13 +19,19 @@ export default function Home() {
       
       <ExperienceBar />
 
-      <section> 
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-      </section>
+      <CountdownProvider>
+        <section> 
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+
       
     </div>
   )
